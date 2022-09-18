5e47ca8dfb470ae7f6d31593f142037b8e117e4a
M jaya suriya <jayasuriya2410@gmail.com>
2022-09-18 12:48:49 +0530

    final commit

--- src/main/java/com/jap/DeleteUpdateStudentData.java
+++ src/main/java/com/jap/DeleteUpdateStudentData.java
@@ -1,14 +1,13 @@
 package com.jap;
 
-import com.jap.model.Student;
 import com.jap.repository.StudentDetails;
 import com.jap.service.StudentService;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * Hello world!
- *
  */
 public class DeleteUpdateStudentData {
     public static void main(String[] args) {
@@ -16,17 +15,17 @@ public class DeleteUpdateStudentData {
             StudentService service = new StudentService();
             service.connect();
             service.printConnectionStatus();
-           StudentDetails studentDetails = new StudentDetails();
+            StudentDetails studentDetails = new StudentDetails();
             Connection connection = service.getConnection();
             System.out.println("----------------------------------------------");
             System.out.println(" Student database : ");
-           studentDetails.getAllStudentDetails(connection);
+            studentDetails.getAllStudentDetails(connection);
             System.out.println("-----------------------------------------------");
             studentDetails.deleteStudentData(connection, 627);
             System.out.println("Student data deleted (roll_number)");
             System.out.println("------------------------------------------------");
             System.out.println("Student data update :");
-            studentDetails.updateStudentData(connection,"suriya",'a',500,625);
+            studentDetails.updateStudentData(connection, "suriya", 'a', 500, 625);
             System.out.println("-------------------------------------------------");
             System.out.println(" Student database :");
             studentDetails.getAllStudentDetails(connection);
