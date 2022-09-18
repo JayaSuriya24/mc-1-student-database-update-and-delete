eddf30017520a377312ca81f8866a7e80cc265ac
M jaya suriya <jayasuriya2410@gmail.com>
2022-09-18 10:59:14 +0530

    getter and setter method is created

--- src/main/java/com/jap/model/student.java
+++ src/main/java/com/jap/model/student.java
@@ -20,4 +20,36 @@ public class student {
         this.grade = grade;
         this.total_marks = total_marks;
     }
+
+    public int getRoll_number() {
+        return roll_number;
+    }
+
+    public void setRoll_number(int roll_number) {
+        this.roll_number = roll_number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGrade() {
+        return grade;
+    }
+
+    public void setGrade(char grade) {
+        this.grade = grade;
+    }
+
+    public int getTotal_marks() {
+        return total_marks;
+    }
+
+    public void setTotal_marks(int total_marks) {
+        this.total_marks = total_marks;
+    }
 }
