994072ff5823088b9a66ae61f52f990991f4181d
M jaya suriya <jayasuriya2410@gmail.com>
2022-09-18 10:26:06 +0530

    values for the table is created

--- src/main/java/com/jap/student.database.upadate.deletequery.sql
+++ src/main/java/com/jap/student.database.upadate.deletequery.sql
@@ -9,3 +9,16 @@ CREATE TABLE IF NOT EXISTS `school`.`student`(
 `grade` char not null,
 `total_marks` int not null
 );
+--query to select all the values in the table
+SELECT * FROM school.student;
+-- query to insert the values inside the table for all the fields
+--query for rollNumber 624
+INSERT INTO `school`.`student` (`roll_number`, `name`, `grade`,`total_marks`) VALUES ('624', 'suriya', 'a','400');
+--query for rollNumber 625
+INSERT INTO `school`.`student` (`roll_number`, `name`, `grade`,`total_marks`) VALUES ('625', 'ajay', 'b','454');
+--query for rollNumber 626
+INSERT INTO `school`.`student` (`roll_number`, `name`, `grade`,`total_marks`) VALUES ('626', 'stanley', 'c','583');
+--query for rollNumber 627
+INSERT INTO `school`.`student` (`roll_number`, `name`, `grade`,`total_marks`) VALUES ('627', 'sugeen', 'c','490');
+--query for rollNumber 628
+INSERT INTO `school`.`student` (`roll_number`, `name`, `grade`,`total_marks`) VALUES ('628', 'hari', 'a','498');
